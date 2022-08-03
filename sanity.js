e982153bcd1e84616f8d3bdf21ccb2c823a13bf3
young zen <youngzenshawty@gmail.com>
2022-08-02 19:15:37 -1000

    ENV

--- sanity.js
+++ sanity.js
@@ -3,13 +3,15 @@ import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_SANITY_DATASET || 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'cqnby4bn',
     apiVersion: "2022-07-26",
     
 
 
     useCdn: process.env.NODE_ENV === "production",
 };
+
+console.log(config)
 
 export const sanityClient = createClient(config);
 
